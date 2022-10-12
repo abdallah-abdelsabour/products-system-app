f387d6adf4ea2c54dbe22c7b1375738f6d296cde
Abdallah_Abdelsabour <abdallah97158@example.com>
2022-10-12 10:53:03 +0200

    create product details page

---
+++ controllers/shop.js
@@ -0,0 +1,55 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
+
+exports.getProductDetails = (req, res, next) => {
+  const productId = req.params.id;
+
+  Product.getProductById(productId, (product) => {
+    console.log(product);
+  });
+
+  res.render("shop/product-detail", {
+    path: "/product-detail",
+    pageTitle: "product details",
+  });
+};
